ac6cb8433c695976d31cc8b3b74a0df56edc87a4
Lucas Santos <lucassantosdev@hotmail.com>
2022-05-31 15:05:06 -0300

    master

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Digite a corrente: ");
+double I = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite a resistencia: ");
+double R = Convert.ToDouble(Console.ReadLine());
+
+double V = R * I;
+
+Console.WriteLine($"A voltagem é {V}");
